14807b37fb16049e703f0c614bb66b67b9c9459c
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-02-09 21:06:07 -0600

    chore: remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
